d0ee3f51102ca3d387ddcae89daa2ee52d29115c
Q2 <quentin.metzler@lunabee.com>
2024-12-06 16:32:47 +0100

    Fix deprectation warning

--- Sources/Bind.swift
+++ Sources/Bind.swift
@@ -67,15 +67,15 @@ where ModelValue.Value == ViewValue.Value, ModelValue.Value: Equatable {
                 guard self.viewValue.wrappedValue != self.modelValue.wrappedValue else { return }
                 self.viewValue.wrappedValue = self.modelValue.wrappedValue
             }
-            .onChange(of: self.modelValue.wrappedValue) {
-                guard self.viewValue.wrappedValue != $0
+            .onChange(of: self.modelValue.wrappedValue) { oldValue, newValue in
+                guard self.viewValue.wrappedValue != newValue
                 else { return }
-                self.viewValue.wrappedValue = $0
+                self.viewValue.wrappedValue = newValue
             }
-            .onChange(of: self.viewValue.wrappedValue) {
-                guard self.modelValue.wrappedValue != $0
+            .onChange(of: self.viewValue.wrappedValue) { oldValue, newValue in
+                guard self.modelValue.wrappedValue != newValue
                 else { return }
-                self.modelValue.wrappedValue = $0
+                self.modelValue.wrappedValue = newValue
             }
     }
 }
