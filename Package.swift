24e3c6d135afbc2e804f4f2001e78735baa1dcfa
Q2 <quentin.metzler@lunabee.com>
2024-12-16 14:30:56 +0100

    Add packages files

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "LBPresenter",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "LBPresenter",
+            targets: ["LBPresenter"]),
+    ],
+    dependencies: [
+        // Add any dependencies here
+    ],
+    targets: [
+        .target(
+            name: "LBPresenter",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "LBPresenterTests",
+            dependencies: ["LBPresenter"]
+        ),
+    ]
+)
